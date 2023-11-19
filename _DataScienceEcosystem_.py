c602373b657b774516521f475338db070477c3ef
dibyanshu02 <124261572+dibyanshu02@users.noreply.github.com>
2023-11-19 11:03:00 +0530

    Add files via upload

---
+++ _DataScienceEcosystem_.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# 
+# In this notebook, we provide a comprehensive summary of the Data Science Tools and Ecosystem, offering insights into the essential tools and technologies shaping the field of data science.
+# 
+
+# # Data Science Languages Overview
+# 
+# Some of the popular languages that Data Scientists use are:
+# 
+# 1. Python : Widely used for its simplicity, readability, and a rich ecosystem of libraries such as NumPy, Pandas, and scikit-learn.
+# 
+# 2. R : Known for statistical computing and graphics, R is favored for its extensive statistical packages and visualization capabilities.
+# 
+# 3. SQL (Structured Query Language) : Essential for working with relational databases, allowing data scientists to query, analyze, and manipulate data stored in databases.
+# 
+# 4. Julia : Gaining popularity for its high-performance numerical and scientific computing, particularly in data-intensive and computational science.
+# 
+# 5. Scala : Often used with Apache Spark for distributed data processing, Scala combines object-oriented and functional programming features.
+# 
+# 
+# 
+
+# # Data Science Libraries Overview
+# 
+# Some of the commonly used libraries by Data Scientists include:
+# 
+# 1. NumPy : A fundamental package for scientific computing in Python, providing support for large, multi-dimensional arrays and matrices, along with mathematical functions.
+# 
+# 2. Pandas : A powerful data manipulation and analysis library, offering data structures like DataFrame for efficient data cleaning, exploration, and analysis.
+# 
+# 3. scikit-learn : A machine learning library that provides simple and efficient tools for data mining and data analysis. It includes various algorithms for classification, regression, clustering, and more.
+# 
+# 4. Matplotlib : A comprehensive library for creating static, animated, and interactive visualizations in Python, commonly used for plotting charts and graphs.
+# 
+# 5. TensorFlow : An open-source machine learning framework developed by Google for building and training deep learning models.
+# 
+# 6. Seaborn : Built on top of Matplotlib, Seaborn is a statistical data visualization library that provides a high-level interface for creating attractive and informative statistical graphics.
+# 
+# 7. Statsmodels : A library for estimating and testing statistical models, including linear and non-linear regression models, time-series analysis, and more.
+# 
+# 8. NLTK (Natural Language Toolkit) : A library for working with human language data, providing easy-to-use interfaces to over 50 corpora and lexical resources.
+# 
+# 9. lxml : A library for processing XML and HTML documents. It provides a simple and efficient way to parse and manipulate structured data.
+# 
+# 10. Plotly : An interactive graphing library that enables the creation of interactive plots and dashboards, suitable for exploratory data analysis.
+# 
+# 
+
+# # Data Science Tools
+# 
+# | Development Environment Tools |
+# | ----------------------------- |
+# | Jupyter Notebooks             |
+# | RStudio                       |
+# | VS Code                       |
+# 
+
+# # Arithmetic Expression Examples
+# 
+# ### Below are a few examples of evaluating arithmetic expressions in Python.
+# 
+
+# In[6]:
+
+
+# This is a simple arithmetic expression to multiply then add integers
+result = (3 * 4) + 5
+result
+
+
+# In[7]:
+
+
+# This will convert 200 minutes to hours by dividing by 60
+minutes = 200
+hours = minutes / 60
+hours
+
+
+# **Objectives:**
+# 
+# - List popular languages for Data Science.
+# - Introduce common libraries used in Data Science.
+# - Showcase development environment open source tools.
+# - Explore examples of evaluating arithmetic expressions in Python.
+# - Demonstrate practical calculations like converting minutes to hours.
+# 
+
+# ## Author
+# 
+# DIBYANSHU
+# 
